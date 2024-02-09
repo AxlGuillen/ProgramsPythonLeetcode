af63aaa2919031d42da30e8aa5b481989d60f637
AxlGuillen <axl1504@hotmail.com>
2024-02-08 22:31:07 -0600

    Begining of the 78

--- Arrays/Easy/1TwoSum.py
+++ Arrays/Easy/1TwoSum.py
@@ -1,5 +1,17 @@
-#My version
 def twoSum(nums, target):
+    """
+    Encuentra dos números dentro de la lista `nums` que sumen `target`.
+
+    Este método utiliza un enfoque de fuerza bruta, comparando cada par de números
+    para encontrar aquellos que sumen el valor objetivo.
+
+    Parámetros:
+    - nums: Lista de enteros.
+    - target: Entero que representa la suma objetivo.
+
+    Imprime el índice de los dos números que suman `target` si se encuentran,
+    de lo contrario imprime un mensaje indicando que no se encontró la suma.
+    """
 
     largo = len(nums)
     con1 = 0
@@ -20,18 +32,36 @@ def twoSum(nums, target):
             con1 += 1
             con2 = con1 + 1
 
-    if encontrado == False:
+    if not encontrado:
         print('No fue encontrada la suma')
 
-#Optimized version
+
 def OptimizedTwoSum(nums, target):
+    """
+    Encuentra dos números dentro de la lista `nums` que sumen `target`.
+
+    Este método utiliza un enfoque optimizado, guardando previamente los números
+    visitados y sus índices en un diccionario para encontrar la pareja buscada
+    en tiempo lineal.
+
+    Parámetros:
+    - nums: Lista de enteros.
+    - target: Entero que representa la suma objetivo.
+
+    Imprime el índice de los dos números que suman `target` si se encuentran.
+    No devuelve nada explícitamente.
+    """
+
     prevMap = {}  # para almacenar valor e índice
     for i, n in enumerate(nums):
         diff = target - n
         if diff in prevMap:
             print([prevMap[diff], i])
+            return  # Añadido para terminar la función una vez se encuentre la pareja
         prevMap[n] = i
-    return
 
-numeros = [3,2,4]
+
+# Uso de la función con ejemplo dado
+numeros = [3, 2, 4]
 OptimizedTwoSum(numeros, 6)
+
