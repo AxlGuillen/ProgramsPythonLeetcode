2b06a9565a1d5a62bf3516d42b3c4f78e897364d
AxlGuillen <axl1504@hotmail.com>
2024-02-29 19:10:13 -0600

    Documentación del 704

--- Arrays/Hard/4MedianOfTwoSortedArrays.py
+++ Arrays/Hard/4MedianOfTwoSortedArrays.py
@@ -1,26 +1,38 @@
-def findMedianSortedArrays( nums1, nums2):
+def findMedianSortedArrays(nums1, nums2):
     """
-    :type nums1: List[int]
-    :type nums2: List[int]
-    :rtype: float
-    que escoga el minimo y el maximo
+    Calcula la mediana de dos listas ordenadas de enteros.
+
+    La función primero encuentra el mínimo y el máximo entre el primer elemento de `nums1` y `nums2` y el último elemento de `nums1` y `nums2`, respectivamente. Luego, calcula el promedio de los medios de cada lista y devuelve el promedio de estos dos promedios como la 'mediana', aunque este no es el cálculo correcto de la mediana para dos listas ordenadas combinadas.
+
+    Parámetros:
+    - nums1: Lista de enteros ordenada.
+    - nums2: Lista de enteros ordenada.
+
+    Devuelve:
+    - Un flotante que representa la 'mediana' calculada de `nums1` y `nums2` según la lógica descrita, que no es el cálculo tradicional de la mediana.
+
+    Nota: Este método no implementa el cálculo correcto de la mediana para dos listas ordenadas combinadas. Para un cálculo correcto, se deberían combinar ambas listas y luego encontrar la mediana de la lista resultante.
     """
+
+    # Longitudes de las listas
     l1 = len(nums1)
     l2 = len(nums2)
 
-    menor = min(nums1[0],nums2[0])
-    maximo = max(nums1[l1-1],nums2[l2-1])
+    # Encuentra el mínimo y máximo entre los primeros y últimos elementos de cada lista
+    menor = min(nums1[0], nums2[0])
+    maximo = max(nums1[l1 - 1], nums2[l2 - 1])
 
-    m1 = (nums1[0] + nums1[l1-1])/2
-    m2 = ((nums2[0] + nums2[l2-1])/2)
+    # Calcula el promedio de los medios de cada lista
+    m1 = (nums1[0] + nums1[l1 - 1]) / 2
+    m2 = (nums2[0] + nums2[l2 - 1]) / 2
 
-    mediana = (m1+m2)/2
+    # Calcula el promedio de los promedios como 'mediana'
+    mediana = (m1 + m2) / 2
 
     return mediana
 
 
-
-nums1 = [1,2]
-nums2 = [3,4]
-
-print(findMedianSortedArrays( nums1, nums2))
+# Ejemplo de uso
+nums1 = [1, 2]
+nums2 = [3, 4]
+print(findMedianSortedArrays(nums1, nums2))
